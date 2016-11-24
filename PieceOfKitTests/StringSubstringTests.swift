06886e612ff6eccb4a5951ef143d99c9a044d0e6
Nico Schuele <nicoschuele@gmail.com>
2016-11-24 02:46:59 +0100

    Remove sample test file

--- PieceOfKitTests/StringSubstringTests.swift
+++ PieceOfKitTests/StringSubstringTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import PieceOfKit
 
 class StringSubstringTests: XCTestCase {
     
--- PieceOfKitTests/StringTrimTests.swift
+++ PieceOfKitTests/StringTrimTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import PieceOfKit
 
 class StringTrimTests: XCTestCase {
     
