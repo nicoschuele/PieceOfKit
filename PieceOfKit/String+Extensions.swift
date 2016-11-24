4acd6dfd479e76492b1eef7b6a2a24354b3a7efa
Nico Schuele <nicoschuele@gmail.com>
2016-11-24 03:00:45 +0100

    Document the substring String extension

--- PieceOfKit/String+Extensions.swift
+++ PieceOfKit/String+Extensions.swift
@@ -14,10 +14,9 @@ public extension String {
     /**
      Trims a string from leading and trailing whitespaces and optionally newline characters
      
-     - Parameters:
-     - newLine: if true, will trim newline characters as well. Defaults to false.
+     - Parameter newLine: if `true`, will trim newline characters as well. Defaults to `false`.
      
-     - Returns: a trimmed String
+     - Returns: a trimmed `String`
      */
     func trim(shouldTrimNewLineCharacters newLine: Bool = false) -> String {
         let string = self
@@ -30,9 +29,16 @@ public extension String {
         }
     }
     
-    func substring(_ r: Range<Int>) -> String {
-        let fromIndex = self.index(self.startIndex, offsetBy: r.lowerBound)
-        let toIndex = self.index(self.startIndex, offsetBy: r.upperBound)
+    /**
+     Gets a substring from a string using a range. Function taken from [http://stackoverflow.com/a/39509186/1296610](http://stackoverflow.com/a/39509186/1296610)
+     
+     - Parameter range: The non inclusive range in the `String` object. For example, `0..<5`
+     
+     - Returns: a substring as `String`
+    */
+    func substring(_ range: Range<Int>) -> String {
+        let fromIndex = self.index(self.startIndex, offsetBy: range.lowerBound)
+        let toIndex = self.index(self.startIndex, offsetBy: range.upperBound)
         return self.substring(with: Range<String.Index>(uncheckedBounds: (lower: fromIndex, upper: toIndex)))
     }
     
