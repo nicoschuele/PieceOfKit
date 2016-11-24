780e73e5b7b1be27da0d613ab59d2f0cf249856c
Nico Schuele <nicoschuele@gmail.com>
2016-11-24 01:50:49 +0100

    Initial Commit

---
+++ PieceOfKit/PieceOfKit.h
@@ -0,0 +1,19 @@
+//
+//  PieceOfKit.h
+//  PieceOfKit
+//
+//  Created by Nico Schuele on 24.11.16.
+//  Copyright © 2016 Nico Schuele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PieceOfKit.
+FOUNDATION_EXPORT double PieceOfKitVersionNumber;
+
+//! Project version string for PieceOfKit.
+FOUNDATION_EXPORT const unsigned char PieceOfKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PieceOfKit/PublicHeader.h>
+
+
