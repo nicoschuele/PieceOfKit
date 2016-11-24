1e0c9031b27c9c4823a7e12510262c3e4538ce58
Nico Schuele <nicoschuele@gmail.com>
2016-11-24 03:56:12 +0100

    Test the length String extension

---
+++ PieceOfKitTests/StringLengthTests.swift
@@ -0,0 +1,21 @@
+//
+//  StringLengthTests.swift
+//  PieceOfKit
+//
+//  Created by Nico Schuele on 24.11.16.
+//  Copyright © 2016 Nico Schuele. All rights reserved.
+//
+
+import XCTest
+@testable import PieceOfKit
+
+class StringLengthTests: XCTestCase {
+    
+    let aString = "This is a test 😌"
+    let theLength = 16
+    
+    func testStringLength() {
+        XCTAssert(aString.length() == theLength)
+    }
+    
+}
