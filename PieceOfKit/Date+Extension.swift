1d090775a931b2206ddc6886281712964ca62dd4
Nico Schuele <nicoschuele@gmail.com>
2016-11-28 05:26:07 +0100

    Add Date extension

---
+++ PieceOfKit/Date+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Date+Extension.swift
+//  PieceOfKit
+//
+//  Created by Nico Schuele on 28.11.16.
+//  Copyright © 2016 Nico Schuele. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    
+    
+    
+}
