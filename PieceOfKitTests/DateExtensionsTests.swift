af0a1ca99b95e41ed635fae8f0c3bf153fedd623
Nico Schuele <nicoschuele@gmail.com>
2016-11-29 20:54:27 +0100

    Refactor and add date manipulation tests

--- PieceOfKitTests/DateExtensionsTests.swift
+++ PieceOfKitTests/DateExtensionsTests.swift
@@ -12,81 +12,90 @@ import XCTest
 class DateExtensionsTests: XCTestCase {
     
     let strInitialDate = "1978-11-03 20:00:00 +0000"
-    let strDatePlusOneMonth = "1978-12-03 20:00:00 +0000"
-    let strDatePlusOneDay = "1978-11-04 20:00:00 +0000"
-    let strDatePlusOneHour = "1978-11-03 21:00:00 +0000"
-    let strDatePlusOneMinute = "1978-11-03 20:01:00 +0000"
-    let strDatePlusOneSecond = "1978-11-03 20:00:01 +0000"
-    let strDateMinusOneMonth = "1978-10-03 20:00:00 +0000"
-    
+    var dateFormatter: DateFormatter! = nil
     var initialDate: Date?
-    var datePlusOneMonth: Date?
-    var datePlusOneDay: Date?
-    var datePlusOneHour: Date?
-    var datePlusOneMinute: Date?
-    var datePlusOneSecond: Date?
-    var dateMinusOneMonth: Date?
     
-    let strYYYYMMDD = "19781103"
-    let strYYYYMMDDHHmm = "197811032000"
-    
-    var dateYYYYMMDD: Date?
-    var dateYYYYMMDDHHmm: Date?
     
     override func setUp() {
         super.setUp()
         
-        let dateFormatter = DateFormatter()
+        dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
-        
         initialDate = dateFormatter.date(from: strInitialDate)
-        datePlusOneMonth = dateFormatter.date(from: strDatePlusOneMonth)
-        datePlusOneDay = dateFormatter.date(from: strDatePlusOneDay)
-        datePlusOneHour = dateFormatter.date(from: strDatePlusOneHour)
-        datePlusOneMinute = dateFormatter.date(from: strDatePlusOneMinute)
-        datePlusOneSecond = dateFormatter.date(from: strDatePlusOneSecond)
-        dateMinusOneMonth = dateFormatter.date(from: strDateMinusOneMonth)
-        
-        let YYYYMMDDFormatter = DateFormatter()
-        YYYYMMDDFormatter.dateFormat = "yyyyMMdd"
-        dateYYYYMMDD = YYYYMMDDFormatter.date(from: strYYYYMMDD)
-        
-        let YYYYMMDDHHmmFormatter = DateFormatter()
-        YYYYMMDDHHmmFormatter.dateFormat = "yyyyMMddHHmm"
-        dateYYYYMMDDHHmm = YYYYMMDDHHmmFormatter.date(from: strYYYYMMDDHHmm)
-
     }
     
     func testAddOneMonth() {
+        let strDatePlusOneMonth = "1978-12-03 20:00:00 +0000"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let datePlusOneMonth = dateFormatter.date(from: strDatePlusOneMonth)
         XCTAssertEqual(datePlusOneMonth!, initialDate?.addMonths(1))
     }
     
     func testAddOneDay() {
+        let strDatePlusOneDay = "1978-11-04 20:00:00 +0000"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let datePlusOneDay = dateFormatter.date(from: strDatePlusOneDay)
         XCTAssertEqual(datePlusOneDay!, initialDate?.addDays(1))
     }
     
     func testAddOneHour() {
+        let strDatePlusOneHour = "1978-11-03 21:00:00 +0000"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let datePlusOneHour = dateFormatter.date(from: strDatePlusOneHour)
         XCTAssertEqual(datePlusOneHour!, initialDate?.addHours(1))
     }
     
     func testAddOneMinute() {
+        let strDatePlusOneMinute = "1978-11-03 20:01:00 +0000"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let datePlusOneMinute = dateFormatter.date(from: strDatePlusOneMinute)
         XCTAssertEqual(datePlusOneMinute!, initialDate?.addMinutes(1))
     }
     
     func testAddOneSecond() {
+        let strDatePlusOneSecond = "1978-11-03 20:00:01 +0000"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let datePlusOneSecond = dateFormatter.date(from: strDatePlusOneSecond)
         XCTAssertEqual(datePlusOneSecond!, initialDate?.addSeconds(1))
     }
     
     func testRemoveOneMonth() {
+        let strDateMinusOneMonth = "1978-10-03 20:00:00 +0000"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let dateMinusOneMonth = dateFormatter.date(from: strDateMinusOneMonth)
         XCTAssertEqual(dateMinusOneMonth!, initialDate?.addMonths(-1))
     }
     
     func testYYYYMMDD() {
-        XCTAssertEqual(dateYYYYMMDD!, Date.dateFromYYYYMMDD(strYYYYMMDD)!)
+        let strYYYYMMDD = "19781103"
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let dateYYYYMMDD = dateFormatter.date(from: strYYYYMMDD)
+        XCTAssertEqual(dateYYYYMMDD!, Date.dateFromString(strYYYYMMDD))
     }
     
     func testYYYYMMDDHHmm() {
-        XCTAssertEqual(dateYYYYMMDDHHmm!, Date.dateFromYYYYMMDDHHmm(strYYYYMMDDHHmm)!)
+        let strYYYYMMDDHHmm = "197811032000"
+        dateFormatter.dateFormat = "yyyyMMddHHmm"
+        let dateYYYYMMDDHHmm = dateFormatter.date(from: strYYYYMMDDHHmm)
+        XCTAssertEqual(dateYYYYMMDDHHmm!, Date.dateFromString(strYYYYMMDDHHmm, stringDateFormat: Date.StringDateFormat.YYYYMMDDHHmm))
+    }
+    
+    func testStringFromDateAsYYYYMMDD() {
+        let stringDate = "19781103"
+        let dateFromString = Date.dateFromString(stringDate)
+        XCTAssertEqual(stringDate, dateFromString!.stringFromDate())
+    }
+    
+    func testStringFromDateAsYYYYMMDDHHmm() {
+        let stringDate = "197811031400"
+        let dateFromString = Date.dateFromString(stringDate, stringDateFormat: .YYYYMMDDHHmm)
+        XCTAssertEqual(stringDate, dateFromString?.stringFromDate(stringDateFormat: .YYYYMMDDHHmm))
+    }
+    
+    func testDateFromStringCanBeNil() {
+        let invalidStringDate = "Hello"
+        let dateFromString = Date.dateFromString(invalidStringDate, stringDateFormat: .YYYYMMDDHHmm)
+        XCTAssertEqual(dateFromString, nil)
     }
     
 }
