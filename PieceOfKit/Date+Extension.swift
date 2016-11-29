160fa6b8f9fba264469cf9d80b5642169cfee783
Nico Schuele <nicoschuele@gmail.com>
2016-11-29 22:21:27 +0100

    Document the Date extensions

--- PieceOfKit/Date+Extension.swift
+++ PieceOfKit/Date+Extension.swift
@@ -8,33 +8,79 @@
 
 import Foundation
 
+/// Date formats for string <--> date conversions
+public enum StringDateFormat {
+    /// A date represented as YYYYMMDD (e.g. 20161129)
+    case YYYYMMDD
+    /// A date represented as YYYYMMDDHHmm (e.g. 201611292219)
+    case YYYYMMDDHHmm
+}
+
+/// Useful extensions for the Date class
 public extension Date {
     
-    public enum StringDateFormat {
-        case YYYYMMDD
-        case YYYYMMDDHHmm
-    }
-    
+    /**
+     Add months to a Date
+     
+     - Parameter months: number of months as `Int`. Accepts negative numbers.
+     
+     - Returns: a `Date`
+     */
     public func addMonths(_ months: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .month, value: months, to: self)!
     }
     
+    /**
+     Add days to a Date
+     
+     - Parameter days: number of days as `Int`. Accepts negative numbers.
+     
+     - Returns: a `Date`
+     */
     public func addDays(_ days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
     
+    /**
+     Add hours to a Date
+     
+     - Parameter hours: number of hours as `Int`. Accepts negative numbers.
+     
+     - Returns: a `Date`
+     */
     public func addHours(_ hours: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .hour, value: hours, to: self)!
     }
     
+    /**
+     Add minutes to a Date
+     
+     - Parameter minutes: number of minutes as `Int`. Accepts negative numbers.
+     
+     - Returns: a `Date`
+     */
     public func addMinutes(_ minutes: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
     }
     
+    /**
+     Add seconds to a Date
+     
+     - Parameter seconds: number of seconds as `Int`. Accepts negative numbers.
+     
+     - Returns: a `Date`
+     */
     public func addSeconds(_ seconds: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .second, value: seconds, to: self)!
     }
     
+    /**
+     Turns a Date into a String 
+     
+     - Parameter format: a `StringDateFormat`
+     
+     - Returns: a `String` in the format YYYYMMDD or YYYYMMDDHHmm
+     */
     public func stringFromDate(stringDateFormat format: StringDateFormat = StringDateFormat.YYYYMMDD) -> String {
         let dateFormatter = DateFormatter()
         
@@ -48,6 +94,13 @@ public extension Date {
         return dateFormatter.string(from: self)
     }
     
+    /**
+     Turns a `String` into a `Date`
+     
+     - Parameter format: the input format of the string. For example, "20161129" is `StringDateFormat.YYYYMMDD`
+     
+     - Returns: a `Date`
+     */
     public static func dateFromString(_ string: String, stringDateFormat format: StringDateFormat = StringDateFormat.YYYYMMDD) -> Date? {
         let dateFormatter = DateFormatter()
         
