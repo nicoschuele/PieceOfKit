22ee0821615d4626f36137ca1b309af16ef89669
Nico Schuele -MB <nicoschuele@gmail.com>
2016-11-30 19:29:18 +0100

    Test the times method in Int extensions

--- PieceOfKitTests/IntExtensionsTests.swift
+++ PieceOfKitTests/IntExtensionsTests.swift
@@ -11,6 +11,11 @@ import XCTest
 
 class IntExtensionsTests: XCTestCase {
     
+    /// function called by x.times tests
+    func addNumbers(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
     func testToString() {
         let a = 3
         let b = 11
@@ -21,4 +26,22 @@ class IntExtensionsTests: XCTestCase {
         XCTAssertEqual(c.toString(), "-1978")
     }
     
+    func testTimes() {
+        var a = 0
+        3.times {
+            a += addNumbers(3, 2)
+        }
+        XCTAssertEqual(a, 15)
+    }
+    
+    func testTimesWithMultipleFunctions() {
+        var a = 0
+        var b = 0        
+        3.times {
+            a += addNumbers(3, 2)
+            b += addNumbers(2, 4)
+        }
+        XCTAssertEqual(a, 15)
+        XCTAssertEqual(b, 18)
+    }
 }
