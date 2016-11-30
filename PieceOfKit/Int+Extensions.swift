d357bf33a3242407193adc5a9e8cd31acfe85a6f
Nico Schuele -MB <nicoschuele@gmail.com>
2016-11-30 19:36:45 +0100

    Document Int extensions

--- PieceOfKit/Int+Extensions.swift
+++ PieceOfKit/Int+Extensions.swift
@@ -8,14 +8,30 @@
 
 import Foundation
 
+/// Useful extensions for the Int class
 public extension Int {
     
+    /**
+     Executes a block multiple times
+     
+     Example:
+     
+            3.times {
+                print("Hello...")
+                print("...you")
+            }
+     */
     public func times (_ function: () -> ()) {
         for _ in 0..<self {
             _ = function()
         }
     }
     
+    /**
+     Converts to `String`
+     
+     - Returns: the `Int` as a `String`
+     */
     public func toString() -> String {
         return "\(self)"
     }
