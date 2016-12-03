7511e696ce99fb3fc979c07ced77bc9cf48e31a7
Nico Schuele <nicoschuele@gmail.com>
2016-12-03 20:00:19 +0100

    Add DateFormatter extensions

---
+++ PieceOfKit/Extensions/DateFormatter+Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  DateFormatter+Extensions.swift
+//  PieceOfKit
+//
+//  Copyright © 2016 Nico Schuele.
+//  Licensed under the Apache License, Version 2.0.
+//
+
+import Foundation
+
+/// Useful extensions for the DateFormatter class
+public extension DateFormatter {
+    
+    /**
+     Returns a `DateFormatter` from a `PieceOfKit.StringDateFormat`
+     
+     - Parameter stringDateFormat: the `StringDateFormat` to use
+     
+     - Returns: a `DateFormatter` corresponding to the `StringDateFormat`
+     */
+    public static func fromStringDateFormat(_ stringDateFormat: StringDateFormat) -> DateFormatter {
+        let dateFormatter = DateFormatter()
+        
+        switch stringDateFormat {
+        case .YYYYMMDD:
+            dateFormatter.dateFormat = "yyyyMMdd"
+        case .YYYYMMDDHHmm:
+            dateFormatter.dateFormat = "yyyyMMddHHmm"
+        case .YYYYMMDDHHmmss:
+            dateFormatter.dateFormat = "yyyyMMddHHmmss"
+        }
+        return dateFormatter
+    }
+    
+}
---
+++ PieceOfKitTests/ExtensionsTests/DateFormatterExtensionsTests.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatterExtensionsTests.swift
+//  PieceOfKit
+//
+//  Copyright © 2016 Nico Schuele.
+//  Licensed under the Apache License, Version 2.0.
+//
+
+import XCTest
+@testable import PieceOfKit
+
+class DateFormatterExtensionsTests: XCTestCase {
+    
+    func testDateFormatterFromStringFormat() {
+        let formatter = DateFormatter.fromStringDateFormat(.YYYYMMDD)
+        XCTAssertEqual(formatter.dateFormat, "yyyyMMdd")
+    }
+    
+}
